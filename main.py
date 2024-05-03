eafa3ec384cd320d132a1239e79fcd8e5346c93c
MilanaErmolina <ermolinamilana053@gmail.com>
2024-05-03 17:21:13 +0300

    Заполнение файлов кодом

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ from handlers import include_routers
 bot = Bot(token="7157439516:AAHTbVmmNgfIu4L0iLJSkBjfqORLcvXZ_tY")
 dp = Dispatcher()
 
+async def main():
+    include_routers(dp)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- states/anketa.py
+++ states/anketa.py
@@ -1,1 +1,7 @@
 
+from aiogram.fsm.state import State, StatesGroup
+
+class Anketa(StatesGroup):
+    name = State()
+    age = State()
+    gender = State()
